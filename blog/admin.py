162657e488a538e9a4b77d2576d56ee3d5b9d18c
wilfredinni <carlos.w.montecinos@gmail.com>
2019-03-12 15:12:34 -0300

    Article and Tag models, Initial migration

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Article, Tag
 
-# Register your models here.
+admin.site.register(Article)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120, unique=True)
+    overview = models.TextField()
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    featured = models.BooleanField(default=False)
+    url = models.SlugField(max_length=120, unique=True)
+    img_url = models.CharField(max_length=200)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
