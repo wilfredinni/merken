2df35fe7dd092d2d35e50cd46478a36cc5f8fb1e
wilfredinni <carlos.w.montecinos@gmail.com>
2019-03-14 19:41:36 -0300

    Added queryset for tag in BlogView

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,20 @@ from django.views.generic import ListView, DetailView
 from .models import Article, Tag
 
 
+def get_all_tags():
+    return Tag.objects.all()
+
+
 class BlogView(ListView):
     model = Article
     context_object_name = "articles"
     template_name = "blog/blog.html"
     paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = get_all_tags()
+        return context
 
 
 class ArticleView(DetailView):
@@ -19,14 +28,14 @@ class ArticleView(DetailView):
 
 class TagView(ListView):
     context_object_name = "articles"
-    template_name = 'blog/blog.html'
+    template_name = "blog/blog.html"
     paginate_by = 5
 
     def get_context_data(self, **kwargs):
-        kwargs['tag'] = self.tag
+        kwargs["tag"] = self.tag
         return super().get_context_data(**kwargs)
 
     def get_queryset(self):
-        self.tag = get_object_or_404(Tag, title=self.kwargs.get('slug'))
+        self.tag = get_object_or_404(Tag, title=self.kwargs.get("slug"))
         queryset = self.tag.article_set.all()
         return queryset
