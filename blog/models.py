a5e832a7053e0e5e32f8df34644616121bbdd657
wilfredinni <carlos.w.montecinos@gmail.com>
2019-03-15 12:35:29 -0300

    Added a base_sidebar template

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Tag(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ["-id"]
+
 
 class Article(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
