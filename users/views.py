4aa918ba05bec91bceefa23e49e1e29056b2d1e1
Carlos <carlos.w.montecinos@gmail.com>
2019-03-17 20:20:32 -0300

    Added author view

--- users/views.py
+++ users/views.py
@@ -1,10 +1,16 @@
 from django.views.generic import DetailView
 
 from .models import CustomUser
+from blog.models import Tag
 
 
 class ProfileView(DetailView):
     model = CustomUser
-    template_name = "users/profile.html"
+    template_name = "users/author.html"
     context_object_name = "user"
     slug_field = "username"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = Tag.objects.all()
+        return context
