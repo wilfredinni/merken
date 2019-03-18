63941526fccfb0ba184e0d1e3b71d2c15b8bf103
Carlos <carlos.w.montecinos@gmail.com>
2019-03-17 21:10:17 -0300

    urls for profile in blog and articles

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 from django.db import models
 
 
@@ -7,6 +8,9 @@ class CustomUser(AbstractUser):
     github = models.CharField(max_length=120, blank=True)
     website = models.CharField(max_length=120, blank=True)
     about = models.TextField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse("users_app:profile", kwargs={"slug": self.username})
 
     def __str__(self):
         return self.username
