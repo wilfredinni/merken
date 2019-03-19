1351505a3ce75ce5aaf8ad5d94a7fdd3271ea817
wilfredinni <carlos.w.montecinos@gmail.com>
2019-03-19 11:03:40 -0300

    Add order for custom pages

--- pages/models.py
+++ pages/models.py
@@ -9,6 +9,7 @@ class Page(models.Model):
     content = models.TextField()
     url = models.SlugField(max_length=120, unique=True)
     visible = models.BooleanField(default=False)
+    order = models.IntegerField(default=0)
     created_at = models.DateTimeField(default=timezone.now)
 
     def get_absolute_url(self):
@@ -16,3 +17,6 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ["order"]
