6023852b66b2f7e17a4232e9c8ed82059a552aea
wilfredinni <carlos.w.montecinos@gmail.com>
2019-03-20 17:05:05 -0300

    Used solo template tags for global configuration

--- dashboard/processors.py
+++ dashboard/processors.py
@@ -1,24 +1,12 @@
 from blog.models import Tag
 from pages.models import Page
-from dashboard.models import SiteConfiguration
 
 
 def global_query(request):
-    SiteConfiguration.get_solo()
-
-    website = SiteConfiguration.objects.get()
-    site_name = website.site_name
-    publisher = website.publisher
-    canonical_url = website.canonical_url
-    site_description = website.site_description
     tags = Tag.objects.all()
     pages = Page.objects.all()
 
     context = {
-        "site_name": site_name,
-        "publisher": publisher,
-        "canonical_url": canonical_url,
-        "site_description": site_description,
         "pages": pages,
         "tags": tags,
     }
