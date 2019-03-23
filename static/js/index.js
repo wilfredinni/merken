b75e69b2224221a408695f353697ba4501d65b6e
wilfredinni <carlos.w.montecinos@gmail.com>
2019-03-23 20:24:43 -0300

    formatting

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,8 @@
 let message = document.getElementById("HomeMessage");
 let closeNotificationBtn = document.getElementById("closeNotification");
 
-
+// functions
 const closeMessage = () => (message.style.display = "none");
+
+// listeners
 closeNotificationBtn.addEventListener("click", closeMessage);
