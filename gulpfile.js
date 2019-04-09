b7e1678e8eb17a496788298287ce80f12d2bae5e
wilfredinni <carlos.w.montecinos@gmail.com>
2019-04-08 21:03:50 -0400

    Sass, source map, minify, autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,26 @@
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssnano = require("cssnano");
+var sourcemaps = require("gulp-sourcemaps");
 
 var paths = {
   styles: {
-    src: "./src/default_template_dev/sass/*.scss",
-    dest: "./src/static_in_env/css"
+    src: "./src/static_in_env/merken/dev/sass/*.scss",
+    dest: "./src/static_in_env/merken/css"
   }
 };
 
 function style() {
   return gulp
     .src(paths.styles.src)
+    .pipe(sourcemaps.init())
     .pipe(sass())
     .on("error", sass.logError)
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.styles.dest));
 }
 
