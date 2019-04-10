90a5a805756b65e51094dbada6e0a6cd756f3c2d
wilfredinni <carlos.w.montecinos@gmail.com>
2019-04-09 22:57:55 -0400

    navbar burger

--- src/static_in_env/merken/js/scroll.js
+++ src/static_in_env/merken/js/scroll.js
@@ -1,26 +1,26 @@
 let scrollButton = document.getElementById("scroll");
 scrollButton.style.display = "none";
-window.onscroll = function () {
-    scrollFunction()
+window.onscroll = function() {
+  scrollFunction();
 };
 
 function scrollFunction() {
-    if (document.documentElement.scrollTop > 200) {
-        scrollButton.style.display = "block"
-    } else {
-        scrollButton.style.display = "none"
-    }
+  if (document.documentElement.scrollTop > 200) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
 }
 let intervalId = 0;
 
 function scrollStep() {
-    if (window.pageYOffset === 0) {
-        clearInterval(intervalId)
-    }
-    window.scroll(0, window.pageYOffset - 2e3)
+  if (window.pageYOffset === 0) {
+    clearInterval(intervalId);
+  }
+  window.scroll(0, window.pageYOffset - 2e3);
 }
 
 function scrollToTop() {
-    intervalId = setInterval(scrollStep, 16.66)
+  intervalId = setInterval(scrollStep, 16.66);
 }
 scrollButton.addEventListener("click", scrollToTop);
