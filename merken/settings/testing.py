c4bfb635d414fff0a820a0442cbaa80f72bea08d
wilfredinni <carlos.w.montecinos@gmail.com>
2019-04-22 19:17:28 -0400

    travis 5

--- merken/settings/testing.py
+++ merken/settings/testing.py
@@ -1,12 +1,4 @@
 from .base import *
 import sys
 
-
-DEBUG = True
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "mydatabase"}}
