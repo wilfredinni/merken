c833370b92ef1109fe1cd47eeaf9831ae405a9d8
wilfredinni <carlos.w.montecinos@gmail.com>
2019-04-23 18:29:53 -0400

    python manage.py env

--- core/management/commands/env.py
+++ core/management/commands/env.py
@@ -2,6 +2,16 @@ from django.core.management.base import BaseCommand
 from merken.settings.base import BASE_DIR
 
 
+# TODO: auto generate SECRET_KEY
+# TODO: DRY
+
+ENV_FIELDS = {
+    "common": ["SECRET_KEY", "DEBUG"],
+    "dev": ["DEV_DB_NAME", "DEV_DB_USERNAME"],
+    "prod": ["DB_NAME", "DB_USERNAME", "DB_PASSWORD", "DB_HOST"],
+}
+
+
 class Command(BaseCommand):
     help = "Creates a .env file for Development or Production, or both."
 
@@ -15,18 +25,71 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         env_for = kwargs["env_file_for"]
 
-        def create_env_file():
-            with open(BASE_DIR + "/.env", 'w') as e:
-                pass
+        def create_env_file(settings):
+            with open(BASE_DIR + "/.env", "w") as e:
+                e.write(settings)
 
         def development():
-            create_env_file()
+            secret_key = input("SECRET_KEY: ")
+            dev_db_name = input("DEV DB NAME: ")
+            dev_db_username = input("DEV DB USERNAME: ")
+
+            env_file = (
+                "DEBUG=True\n"
+                f"SECRET_KEY={secret_key}\n\n"
+                f"DEV_DB_NAME={dev_db_name}\n"
+                f"DEV_DB_USERNAME={dev_db_username}"
+            )
+            # print(env_file)
+            create_env_file(env_file)
 
         def production():
-            print("production")
+            secret_key = input("SECRET_KEY: ")
+            db_username = input("DB NAME: ")
+            db_username = input("DB USERNAME: ")
+            db_password = input("DB PASSWORD: ")
+            db_host = input("DB HOST: ")
+
+            env_file = (
+                "DEBUG=False\n"
+                f"SECRET_KEY={secret_key}\n\n"
+                f"DB_NAME={db_username}\n"
+                f"DB_USERNAME={db_username}\n"
+                f"DB_PASSWORD={db_password}\n"
+                f"DB_HOST={db_host}"
+            )
+            # print(env_file)
+            create_env_file(env_file)
 
         def both():
-            print("both")
+            # commons
+            debug = input("DEBUG: ")
+            secret_key = input("SECRET_KEY: ")
+
+            # development
+            dev_db_name = input("DEV DB NAME: ")
+            dev_db_username = input("DEV DB USERNAME: ")
+
+            # production
+            db_username = input("DB NAME: ")
+            db_username = input("DB USERNAME: ")
+            db_password = input("DB PASSWORD: ")
+            db_host = input("DB HOST: ")
+
+            env_file = (
+                # common
+                f"DEBUG={debug}\n"
+                f"SECRET_KEY={secret_key}\n\n"
+                # development
+                f"DEV_DB_NAME={dev_db_name}\n"
+                f"DEV_DB_USERNAME={dev_db_username}\n\n"
+                # production
+                f"DB_NAME={db_username}\n"
+                f"DB_USERNAME={db_username}\n"
+                f"DB_PASSWORD={db_password}\n"
+                f"DB_HOST={db_host}"
+            )
+            create_env_file(env_file)
 
         if env_for == "dev":
             development()
