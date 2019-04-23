b483033aea5eff0fa9937653bc7263ca6031fb8e
wilfredinni <carlos.w.montecinos@gmail.com>
2019-04-23 00:10:50 -0400

    Default Value decouple

--- merken/settings/base.py
+++ merken/settings/base.py
@@ -7,7 +7,9 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config(
+    "SECRET_KEY", default="$9597jcpibr3w!$(y^lm+77qp()*wc^ty%ak4v!g(@1=9p!^kp"
+    )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
