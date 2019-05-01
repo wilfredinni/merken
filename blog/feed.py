f67e413d9fb76479c4f01c501c91770fd3b9c17b
wilfredinni <carlos.w.montecinos@gmail.com>
2019-05-01 19:52:40 -0400

    Fixed RSS feed

--- blog/feed.py
+++ blog/feed.py
@@ -1,28 +1,30 @@
 from django.contrib.syndication.views import Feed
 
+from dashboard.models import SiteConfiguration
 from .models import Article
 
-# TODO: error when loading siteconfiguration, load settings form
-# SiteConfiguration
-# from dashboard.models import SiteConfiguration
+from mistune import markdown
 
 
 class ArticleFeed(Feed):
-    # site_info = SiteConfiguration.objects.get(pk=1)
+    try:
+        site_info = SiteConfiguration.load()
+    except:
+        pass
 
-    title = f"Python Cheatsheet Blog Feed"  # replace with SiteConfiguration
     link = "/blog/"
     description = (  # replace with SiteConfiguration
         f"Updates to the Python Cheatsheet Blog"
     )
 
+    def title(self):
+        return f"{self.site_info.site_name}"
+
     def items(self):
-        return Article.objects.all()[:5]
+        return Article.objects.all()[:20]
 
     def item_title(self, item):
         return item.title
 
     def item_description(self, item):
-        return item.overview
-
-    pass
+        return markdown(item.overview)
