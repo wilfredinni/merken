255db15b5d66830962e7b990e6939651ef612409
wilfredinni <carlos.w.montecinos@gmail.com>
2019-05-02 12:04:49 -0400

    feed

--- blog/feed.py
+++ blog/feed.py
@@ -1,24 +1,21 @@
 from django.contrib.syndication.views import Feed
 
-from dashboard.models import SiteConfiguration
+# from dashboard.models import SiteConfiguration
 from .models import Article
 
 from mistune import markdown
 
 
 class ArticleFeed(Feed):
-    try:
-        site_info = SiteConfiguration.load()
-    except:
-        pass
-
+    # site_info = SiteConfiguration.load()
     link = "/blog/"
     description = (  # replace with SiteConfiguration
         f"Updates to the Python Cheatsheet Blog"
     )
 
     def title(self):
-        return f"{self.site_info.site_name}"
+        # return f"{self.site_info.site_name}"
+        return "Python Cheatsheet"
 
     def items(self):
         return Article.objects.all()[:20]
