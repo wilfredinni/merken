e2144d687ceb749917a7ecd90faa1ba6fcbf9a7a
wilfredinni <carlos.w.montecinos@gmail.com>
2019-05-21 19:49:02 -0400

    logging with sentry on productrion

--- merken/settings/production.py
+++ merken/settings/production.py
@@ -1,5 +1,14 @@
-from .base import *
+import sentry_sdk
 from decouple import config
+from sentry_sdk.integrations.django import DjangoIntegration
+
+from .base import *
+
+sentry_dsn = config("SENTRY_DSN")
+sentry_sdk.init(
+    dsn=sentry_dsn,
+    integrations=[DjangoIntegration()]
+)
 
 DEBUG = False
 ALLOWED_HOSTS = ["127.0.0.1"]  # ie. ["ip-address", "www.site.com"]
