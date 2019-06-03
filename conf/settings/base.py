fba1f2f2e7233a29e1b67cd25075eb6b69f17070
wilfredinni <carlos.w.montecinos@gmail.com>
2019-06-02 21:48:17 -0400

    settings

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -95,6 +95,9 @@ TEMPLATES = [
     }
 ]
 
+# -----------------------------------------------------------------------------
+# Rest Framework
+# -----------------------------------------------------------------------------
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
--- conf/settings/production.py
+++ conf/settings/production.py
@@ -4,14 +4,19 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 from .base import *
 
+
+DEBUG = False
+ALLOWED_HOSTS = ["127.0.0.1"]  # ie. ["ip-address", "www.site.com"]
+
+# -----------------------------------------------------------------------------
+# Logging
+# -----------------------------------------------------------------------------
+
 sentry_dsn = config("SENTRY_DSN")
 sentry_sdk.init(
     dsn=sentry_dsn,
     integrations=[DjangoIntegration()]
 )
-
-DEBUG = False
-ALLOWED_HOSTS = ["127.0.0.1"]  # ie. ["ip-address", "www.site.com"]
 
 # -----------------------------------------------------------------------------
 # Databases
