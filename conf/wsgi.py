c9cf833dbaab6e046a2cd29c6229bd1b6d8053d1
carlos <carlos.w.montecinos@gmail.com>
2020-07-02 11:18:45 -0400

    caprover fix

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -2,5 +2,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 application = get_wsgi_application()
